8a395f789aa850733f209a035f08e0e03bd2b3db
Dustin Firebaugh <dafirebaugh@gmail.com>
2024-05-15 11:06:54 -0400

    fix imggen to print actual data

--- pkg/imggen/imggen.go
+++ pkg/imggen/imggen.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"math"
+	"math/rand"
 
 	"github.com/dfirebaugh/planjam/pkg/plan"
 	"github.com/dfirebaugh/tortuga/pkg/imagefb"
@@ -33,6 +35,7 @@ func Gen(boardLabel string, b *plan.Board) *image.RGBA {
 	printAt(fb, fmt.Sprintf("Plan Jam Report -- Board: %s", boardLabel), 5, letterYOffset, colornames.Greenyellow)
 
 	printBarChart(fb, 0, 30, b)
+	printPieChart(fb, 350, 50, 50, b)
 
 	img := image.NewRGBA(image.Rect(0, 0, imageWidth, imageHeight))
 	img.Pix = fb.GetFrame()
@@ -72,4 +75,51 @@ func countTotalFeatures(b *plan.Board) int {
 		total += len(lane.Features)
 	}
 	return total
+}
+
+func printPieChart(fb Display, centerX int, centerY int, radius int, b *plan.Board) {
+	totalFeatures := countTotalFeatures(b)
+	if totalFeatures == 0 {
+		return
+	}
+
+	startAngle := 0.0
+
+	for _, lane := range b.Lanes {
+		laneFeatureCount := len(lane.Features)
+		angle := 2 * math.Pi * float64(laneFeatureCount) / float64(totalFeatures)
+		drawPieSlice(fb, centerX, centerY, radius, startAngle, startAngle+angle, randomColor())
+		startAngle += angle
+	}
+}
+
+func drawPieSlice(fb Display, centerX int, centerY int, radius int, startAngle float64, endAngle float64, clr color.RGBA) {
+	for angle := startAngle; angle < endAngle; angle += 0.01 {
+		x1 := centerX + int(float64(radius)*math.Cos(angle))
+		y1 := centerY + int(float64(radius)*math.Sin(angle))
+		x2 := centerX + int(float64(radius)*math.Cos(angle+0.01))
+		y2 := centerY + int(float64(radius)*math.Sin(angle+0.01))
+
+		line1 := geom.MakeLine(geom.Point{X: float64(centerX), Y: float64(centerY)}, geom.Point{X: float64(x1), Y: float64(y1)})
+		line2 := geom.MakeLine(geom.Point{X: float64(x1), Y: float64(y1)}, geom.Point{X: float64(x2), Y: float64(y2)})
+		line3 := geom.MakeLine(geom.Point{X: float64(x2), Y: float64(y2)}, geom.Point{X: float64(centerX), Y: float64(centerY)})
+
+		line1.Draw(fb, clr)
+		line2.Draw(fb, clr)
+		line3.Draw(fb, clr)
+	}
+}
+
+func randomColor() color.RGBA {
+	colors := []color.RGBA{
+		colornames.Red,
+		colornames.Green,
+		colornames.Blue,
+		colornames.Yellow,
+		colornames.Orange,
+		colornames.Purple,
+		colornames.Cyan,
+		colornames.Magenta,
+	}
+	return colors[rand.Intn(len(colors))]
 }
